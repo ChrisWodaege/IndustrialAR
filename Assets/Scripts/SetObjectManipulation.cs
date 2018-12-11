dc37258cb42738c5281bf7e2aab53baf4bd4c97c
Chris Wodäge <chris_wodaege@web.de>
2018-12-11 14:54:07 +0100

    working

---
+++ Assets/Scripts/SetObjectManipulation.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+using HoloToolkit.Unity.InputModule.Tests;
+using HoloToolkit.Unity.InputModule.Utilities.Interactions;
+
+public class SetObjectManipulation : MonoBehaviour, IInputClickHandler
+{
+    public GameObject WeaManipulator;
+    private string name;
+
+    void Start()
+    {
+        name = gameObject.name;
+       
+    }
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        if(name == "Move")
+        {
+            WeaManipulator.GetComponent<HandDraggable>().enabled = true;
+            WeaManipulator.GetComponent<NavigationRotateResponder>().enabled = false;
+            WeaManipulator.GetComponent<TwoHandManipulatable>().enabled = false;
+        }
+
+        if (name == "Rotate")
+        {
+            WeaManipulator.GetComponent<HandDraggable>().enabled = false;
+            WeaManipulator.GetComponent<NavigationRotateResponder>().enabled = true;
+            WeaManipulator.GetComponent<TwoHandManipulatable>().enabled = false;
+        }
+
+        if (name == "Scale")
+        {
+            WeaManipulator.GetComponent<HandDraggable>().enabled = false;
+            WeaManipulator.GetComponent<NavigationRotateResponder>().enabled = false;
+            WeaManipulator.GetComponent<TwoHandManipulatable>().enabled = true;
+        }
+    }
+}
